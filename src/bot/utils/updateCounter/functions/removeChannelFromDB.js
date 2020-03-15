d4d0422ffeeb758956273fff84c356dff3fd573e
eduardozgz <eduardozgz@protonmail.com>
2020-03-15 14:24:59 +0100

    Tweaks

--- src/bot/utils/updateCounter/functions/removeChannelFromDB.js
+++ src/bot/utils/updateCounter/functions/removeChannelFromDB.js
@@ -6,7 +6,7 @@ module.exports = async ({ client, channelId, type, guildSettings, error, forceRe
         `I tried to update ${guildId}'s counter (channel: ${channelId}), but I don't have the proper permissions or it does not exists.${(error) ? "Error code:" + error.code : "" }`
     );
 
-    if (forceRemove || error && (error.code === 50013 || error.code === 50001 || error.code === 10003 || error.code === 40001)) {
+    if (forceRemove || error && (error.code === 50013 || error.code === 10003 || error.code === 40001)) {
         switch (type) {
             case "topicCounter":
                 GuildModel.findOneAndUpdate({ guild_id: guildId }, {
